9f4fed21fe0a89c77ef70ae8d4d36d5c7d52f5e3
Deep <deepugupta930@gmail.com>
2024-10-22 13:57:52 +0530

    config update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     applyCssImports: true,
+//   },
+//   basePath: '/github-pages',
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  experimental: {
-    applyCssImports: true,
-  },
-  basePath: '/github-pages',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
